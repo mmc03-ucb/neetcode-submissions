3c6cdff1e2ed6642f066503256e1a4b1327a611f
Muqueet Mohsen Chowdhury <70145981+mmc03-ucb@users.noreply.github.com>
2026-07-20 07:47:09 +1000

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.py
@@ -0,0 +1,37 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        def compare(s_count, t_count):
+            for letter, count in t_count.items():
+                if s_count[letter] < count:
+                    return False
+            return True
+
+        if len(s) == len(t):
+            if Counter(s) == Counter(t):
+                return s
+            else:
+                return ""
+        elif len(s) < len(t):
+            return ""
+        else:
+            print("enter else")
+            t_count = Counter(t)
+            l = 0
+            s_count = Counter()
+            length = float("INF")
+            shortest = ""
+            for r in range(len(s)):
+                s_count[s[r]] += 1
+                while (r - l + 1) >= len(t) and compare(s_count, t_count):
+                    print(s_count)
+                    print(s[l:r+1])
+                    if (r - l + 1) < length:
+                        length = r - l + 1
+                        shortest = s[l: r + 1]
+                    s_count[s[l]] -= 1
+                    if s_count[s[l]] == 0:
+                        del s_count[s[l]]
+                    l += 1
+
+            return shortest
+
