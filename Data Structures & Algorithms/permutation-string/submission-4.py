cca396b089086311f3b0f5e99e44f09884207cd9
Muqueet Mohsen Chowdhury <70145981+mmc03-ucb@users.noreply.github.com>
2026-07-22 10:12:19 +1000

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.py
@@ -0,0 +1,24 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s2) < len(s1):
+            return False
+        
+        s1_count = Counter(s1)
+        s2_count = Counter()
+
+        l = 0
+
+        for r in range(len(s2)):
+            s2_count[s2[r]] += 1
+
+            if (r - l + 1) > len(s1):
+                s2_count[s2[l]] -= 1
+                l += 1
+                if s2_count[s2[l]] == 0:
+                    del s2_count[s2[l]]
+            
+            if s1_count == s2_count:
+                return True
+        
+        return False
+            
